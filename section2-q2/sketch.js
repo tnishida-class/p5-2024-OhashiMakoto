582cbb9366cc4c70e5e70bec4b4176f13599c3a0
OhashiMakoto <makoto197555@icloud.com>
2024-10-22 11:16:10 +0900

    change 2-q2,q3

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,22 +3,22 @@ function setup() {
   let d = 40;
   createCanvas(400, 400);
   background(255);
-noStroke();
+  noStroke();
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      x = i * s; 
-      y = j * s; 
+      let x = i * s;
+      let y = j * s;
       if ((i + j) % 2 === 1) {
- 塗りつぶし(122);
-rect(x, y, s, s);
+        fill(122);
+        rect(x, y, s, s);
       }
       if (((i + j) % 2 === 1) && (j < 3)) {
- 塗りつぶし(255, 0, 0);
- 楕円(x + s / 2, y + s / 2, d); 
+        fill(255, 0, 0);
+        ellipse(x + s / 2, y + s / 2, d);
       }
       if (((i + j) % 2 === 1) && (4 < j)) {
- 塗りつぶし(0);
- 楕円(x + s / 2,y + s / 2,d,d); 
+        fill(0);
+        ellipse(x + s / 2, y + s / 2, d, d);
       }
     }
   }
--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,10 +1,13 @@
-// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
-
+let size = 50;// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
+let count =0;
+let cycle = 100;
+let increment = 1;
 function setup(){
   createCanvas(200, 200);
-  count = 0;
+}
+function draw(){
+  background(160, 192, 255);
+  count = 
   cycle = 100;
 }
 
